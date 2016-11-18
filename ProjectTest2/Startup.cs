03c7e0933f5dd526e7dd6b94630a3ae048988d3c
Digiday <Digiday@DIGIDAY-PC>
2016-11-18 16:11:36 +0700

    Add project files.

---
+++ ProjectTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectTest2.Startup))]
+namespace ProjectTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
